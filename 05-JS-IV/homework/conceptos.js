05bace395867224d054d895292c9c3c038809bbb
Car <carla_defazy@hotmail.com>
2021-10-14 10:58:34 -0300

    homework

---
+++ 05-JS-IV/homework/conceptos.js
@@ -0,0 +1,8 @@
+//Objetos: es una colección de propiedades y una propiedad es una asociación entre un nombre 
+// (o clave) y unvalor.
+//Métodos: cuando una propiedad sea una función, la llamamos Método.
+//Bucle for...in:recorre cada clave del objeto y finaliza cuando se hayan iterado
+// todas las claves.
+//Notación de puntos: Para acceder a las propiedades escribiendo el nombre+punto+nombredelapropiedad.
+// Notación de corchetes: es como llamar a un elemento en una matriz, aunque con [] y 
+// entre comillas.
