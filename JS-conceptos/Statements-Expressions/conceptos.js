423022ce6af75e7448cb62e44a90bd3ee1075c8b
Car <carla_defazy@hotmail.com>
2021-10-15 10:45:37 -0300

    homework

---
+++ JS-conceptos/Statements-Expressions/conceptos.js
@@ -0,0 +1,3 @@
+// prototype: todos los objetos de JS tienen (en una referencia) a otro objeto
+//que es llamado su prototipo; heredan características entre sí.
+// Constructors: son un método para crear un objeto creado a partir de una clase.
